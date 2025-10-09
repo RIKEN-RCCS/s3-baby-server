b71b7341c544c39921f653d054fa834f3e5ca63e
zzmatu <zzmatu@users.noreply.github.com>
2025-10-09 18:16:17 +0900

    ()

---
+++ test/minima/prepare-s3cli.sh
@@ -0,0 +1,21 @@
+#!/bin/ksh
+
+# This creates files needed to run the "bottom" test.
+
+# (1) Make a file of 4MB, 10MB.
+
+rm -f data-04m.txt
+touch data-04m.txt
+shred -n 1 -s 4M data-04m.txt
+
+rm -f data-10m.txt
+touch data-10m.txt
+shred -n 1 -s 10M data-10m.txt
+
+if [ ! -d files ]; then
+    mkdir ./files
+fi
+cp data-10m.txt ./files/data-001.txt
+cp data-10m.txt ./files/data-002.txt
+cp data-10m.txt ./files/data-003.txt
+cp data-10m.txt ./files/data-004.txt
