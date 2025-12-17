0ef79589e53849fd7954865c3c24cfb33c95667d
zzmatu <zzmatu@users.noreply.github.com>
2025-12-18 00:21:05 +0900

    ()

---
+++ v1/server/auxiliary2_test.go
@@ -0,0 +1,69 @@
+package server
+
+import (
+	//"encoding/xml"
+	"fmt"
+	//"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+	//"io"
+	"log"
+	"strings"
+	"testing"
+	//"time"
+)
+
+var body1 = `<CompleteMultipartUpload xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+  <Part>
+    <ETag>"etag1"</ETag>
+    <PartNumber>1</PartNumber>
+  </Part>
+  <Part>
+    <ETag>"etag2"</ETag>
+    <PartNumber>2</PartNumber>
+  </Part>
+</CompleteMultipartUpload>`
+
+var body2 = `<Delete xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+  <Object>
+    <ETag>etag1</ETag>
+    <Key>key1</Key>
+    <LastModifiedTime>2006-01-02T15:04:05Z</LastModifiedTime>
+    <Size>4096</Size>
+    <VersionId></VersionId>
+  </Object>
+  <Object>
+    <ETag>etag2</ETag>
+    <Key>key2</Key>
+    <LastModifiedTime>2006-01-02T15:04:05Z</LastModifiedTime>
+    <Size>1024</Size>
+    <VersionId></VersionId>
+  </Object>
+  <Quiet>false</Quiet>
+</Delete>`
+
+func TestXmlMarshal2(t *testing.T) {
+	fmt.Printf("Test XML Marshaling 2...\n")
+	{
+		var r = strings.NewReader(body1)
+
+		var o O_CompletedMultipartUpload
+		var err1 = h_decode_body(&o, r, nil)
+		if err1 != nil {
+			log.Fatalf("Decode() error: %v", err1)
+		}
+		var x *types.CompletedMultipartUpload = import_CompletedMultipartUpload(&o)
+		fmt.Printf("Body x=%#v\n", x)
+	}
+
+	{
+		var r = strings.NewReader(body2)
+
+		var o O_Delete
+		var err1 = h_decode_body(&o, r, nil)
+		if err1 != nil {
+			log.Fatalf("Decode() error: %v", err1)
+		}
+		var x *types.Delete = import_Delete(&o)
+		fmt.Printf("Body x=%#v\n", x)
+	}
+}
