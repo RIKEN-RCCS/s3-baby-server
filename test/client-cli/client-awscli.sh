6ae896588cf16faaf86d6991828976951837a090
zzmatu <zzmatu@users.noreply.github.com>
2026-02-19 01:25:15 +0900

    ()

---
+++ test/client-cli/client-awscli.sh
@@ -0,0 +1,33 @@
+#!/bin/ksh
+
+. ./cli-fn.sh
+
+CLIGET="aws s3 cp --no-verify-ssl --no-cli-pager --no-progress"
+CLIPUT="aws s3 cp --no-verify-ssl --no-cli-pager --no-progress"
+CLILS="aws s3 ls --no-verify-ssl --no-cli-pager"
+CLIMV="aws s3 mv --no-verify-ssl --no-cli-pager"
+CLIRM="aws s3 rm --no-verify-ssl --no-cli-pager"
+CLIMB="aws s3 mb --no-verify-ssl --no-cli-pager"
+CLIRB="aws s3 rb --no-verify-ssl --no-cli-pager"
+
+EXEC_ECHO aws s3 ls --no-verify-ssl --no-cli-pager s3://
+EXEC_ECHO aws s3 mb --no-verify-ssl --no-cli-pager s3://mybucket1 || true
+
+. ./copy-copy.sh
+
+ECHO "Test mv"
+
+EXEC_ECHO ${CLIPUT} data-01k.txt s3://mybucket1/object1.txt
+EXEC_ECHO ${CLIMV} s3://mybucket1/object1.txt s3://mybucket1/object2.txt
+EXEC_ECHO ${CLIGET} s3://mybucket1/object2.txt "zzz1"
+EXEC_ECHO cmp "zzz1" data-01k.txt
+rm -f "zzz1"
+
+ECHO "Clean up"
+
+EXEC_ECHO ${CLIRM} s3://mybucket1/object2.txt
+EXEC_ECHO ${CLIRB} s3://mybucket1
+
+rm -f "zzz1"
+
+ECHO_TEST_DONE
